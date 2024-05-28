df16f28cea82f7227d3a075acd7d44bf6585d737
tazman230a <76573669+tazman230a@users.noreply.github.com>
2024-05-28 16:38:04 -0700

    first commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { FaLocationArrow } from "react-icons/fa6";
 
 import { socialMedia } from "@/data";
@@ -5,15 +6,7 @@ import MagicButton from "@/components/ui/MagicButton";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
-        <div className="w-full absolute left-0 -bottom-72 min-h-96">
-            <img 
-               src=" /footer-grid.svg"
-               alt="grid"
-               className=" w-full h-full opacity-50" 
-               />
-        </div>
-
+    <footer className="w-full mb-[100px] md:mb-5 pb-10" id="contact">
         <div className="flex flex-col items-center">
             <h1 className="heading lg:max-w-[45vw]">
                 Ready to take <span className="text-purple">your</span> digital presence to the next level?
